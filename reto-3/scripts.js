a524ddfd6607cc9d4c10881a4820c024edc709fd
luisgentil <luisgentilalperiz@gmail.com>
2026-06-30 20:27:42 +0200

    Creación del proyecto Reto 3

---
+++ reto-3/scripts.js
@@ -0,0 +1,19 @@
+
+
+
+    function greet() {
+alert("¡Hola, mundo!");
+    }
+
+
+    function cambiaColor() {
+        $("p").css("color", "blue");
+    }
+
+function agregarTexto() {
+    $("p").append(" Cada click agrega más texto. ");
+}
+
+function ocultar() {
+    $("p").fadeToggle();
+}
